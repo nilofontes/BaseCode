a81d6cd6e5d88f8d6ff7a5ecfb63d9e3043fddc0
DESKTOP-2DAASE1\nilof <nilo.fontes@hotmail.com>
2024-05-04 01:16:14 -0300

    Arrumando git

--- Models/LoginRequest.cs
+++ Models/LoginRequest.cs
@@ -2,7 +2,6 @@
 {
     public class LoginRequest
     {
-        var teste = "123";
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string PassWordHash { get; set; }
